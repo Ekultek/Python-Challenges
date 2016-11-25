a73ab34718dd23aff3ab4bfef65c2e6095154b9b
Ekultek <thomas.perkins23@icloud.com>
2016-11-25 16:31:16 -0600

    initial commit

--- codeEval/lower.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-
-if __name__ == '__main__':
-    with open(sys.argv[1], "r") as strings:
-        for string in strings:
-            print string.lower()
--- codeEval/sum.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-
-if __name__ == '__main__':
-    with open(sys.argv[1], "r") as numbers:
-        for num in numbers:
-            arr = list(num.rstrip())
-            print sum(int(i) for i in arr)
